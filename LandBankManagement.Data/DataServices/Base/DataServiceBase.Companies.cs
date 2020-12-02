db421cbc6b657307f247a3146ed3866b510f7232
mdhanajeyan <jeyan10@gmail.com>
2020-12-02 19:21:52 +0530

    Splash sccreen - simple change Use BuildSearchTerms instead of Search Terms

--- LandBankManagement.Data/DataServices/Base/DataServiceBase.Companies.cs
+++ LandBankManagement.Data/DataServices/Base/DataServiceBase.Companies.cs
@@ -64,7 +64,7 @@ namespace LandBankManagement.Data.Services
             // Query
             if (!String.IsNullOrEmpty(request.Query))
             {
-                items = items.Where(r => r.SearchTerms.Contains(request.Query.ToLower()));
+                items = items.Where(r => r.BuildSearchTerms().Contains(request.Query.ToLower()));
             }
 
             // Where
@@ -110,7 +110,7 @@ namespace LandBankManagement.Data.Services
             // Query
             if (!String.IsNullOrEmpty(request.Query))
             {
-                items = items.Where(r => r.SearchTerms.Contains(request.Query.ToLower()));
+                items = items.Where(r => r.BuildSearchTerms().Contains(request.Query.ToLower()));
             }
 
             // Where
