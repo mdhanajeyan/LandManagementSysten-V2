f9491c1bfc248f8aa9ce9c10a3a1256d3d4bad12
mad4pals <mailmadhusudhan@gmail.com>
2020-12-07 14:56:31 +0530

    Area Calculation extension

---
+++ LandBankManagement.App/Converters/AreaConvertor.cs
@@ -0,0 +1,22 @@
+﻿using LandBankManagement.Data.Data;
+using LandBankManagement.Extensions;
+
+namespace LandBankManagement.Converters
+{
+    public class AreaConvertor
+    {
+        public Area ConvertArea(decimal acres, decimal guntas)
+        {
+            var totalGuntas = acres.AcreToGuntas() + guntas;
+            var totalSqFt = totalGuntas.GuntasToSqft();
+            var area = new Area()
+            {
+                Acres = totalSqFt.SqftToAcres(),
+                Guntas = totalSqFt.SqftToGuntas(),
+                SqMeters = totalSqFt.SqftToSqMts(),
+                SqFt = totalSqFt
+            };
+            return area;
+        }
+    }
+}
---
+++ LandBankManagement.Data/Data/Area.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandBankManagement.Data.Data
+{
+    public class Area
+    {
+        public decimal Acres { get; set; }
+        public decimal PartGunta { get; set; }
+        public decimal Guntas { get; set; }
+        public decimal SqMeters { get; set; }
+        public decimal SqFt { get; set; }
+    }
+}
