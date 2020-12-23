77e5a834e0bbc680b1ef724e1e75e603190dbf51
mad4pals <mailmadhusudhan@gmail.com>
2020-12-23 18:48:16 +0530

    PropertyMerge

---
+++ LandBankManagement.Data/Data/PropertyMerge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandBankManagement.Data.Data
+{
+	public class PropertyMerge
+    {
+        public int PropertyMergeId { get; set; }
+        public Guid PropertyMergeGuid { get; set; }
+        public string PropertyMergeDealName { get; set; }
+        public decimal MergedTotalArea { get; set; }
+        public decimal MergedSaleValue1 { get; set; }
+        public decimal MergedSaleValue2 { get; set; }
+        public decimal MergedAmountPaid1 { get; set; }
+        public decimal MergedAmountPaid2 { get; set; }
+        public decimal MergedBalancePayable1 { get; set; }
+        public decimal MergedBalancePayable2 { get; set; }
+        public bool ForProposal { get; set; }
+	}
+}
---
+++ LandBankManagement.Data/Data/PropertyMergeList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandBankManagement.Data.Data
+{
+	public class PropertyMergeList
+    {
+        public int PropertyMergeListId { get; set; }
+        public Guid PropertyMergeGuid { get; set; }
+        public Guid PropertyGuid { get; set; }
+	}
+}
