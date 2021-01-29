6118e78631c0e075960f9e1a6055ed718b0b92a4
Madhu Sudhan <madhusudhan@gmail.com>
2021-01-29 13:11:14 +0530

    area calculator update

--- LandBankManagement.App/Converters/AreaConvertor.cs
+++ LandBankManagement.App/Converters/AreaConvertor.cs
@@ -1,4 +1,5 @@
-﻿using LandBankManagement.Data.Data;
+﻿using System;
+using LandBankManagement.Data.Data;
 using LandBankManagement.Extensions;
 
 namespace LandBankManagement.Converters
@@ -18,6 +19,25 @@ namespace LandBankManagement.Converters
                 Anas=aanas,
                 SqFt = totalSqFt
             };
+
+            if (area.Anas >= 16)
+            {
+                var remainder = area.Anas % 16 ;
+                int quotient = Convert.ToInt32(area.Anas / 16);
+                
+                area.Anas = remainder;
+                area.Guntas += quotient;
+            }
+
+            if (area.Guntas >= 40)
+            {
+                var remainder = area.Guntas % 40;
+                int quotient = Convert.ToInt32(area.Guntas / 40);
+
+                area.Guntas = remainder;
+                area.Acres += quotient;
+            }
+            
             return area;
         }
     }
